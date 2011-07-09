362884494c984211e2d1f2679d44a12534757289
Patrick Guido Arminio <patrick.arminio@gmail.com>
2011-07-09 11:00:58 +0200

    Added wmode opaque to the soudcloud player

--- soundcloud_importer/mes_fonctions.php
+++ soundcloud_importer/mes_fonctions.php
@@ -12,6 +12,7 @@ function soundcloud_player($url, $show_comments=false, $auto_play=false,
 <object width="$width" height="$height">
 <param value="http://player.soundcloud.com/player.swf?url=$url&show_comments=$show_comments&auto_play=$auto_play&color=$color" name="movie">
 <param value="always" name="allowscriptaccess">
+<param name="wmode" value="opaque" />
 <embed width="$width" height="$height" type="application/x-shockwave-flash" src="http://player.soundcloud.com/player.swf?url=$url&show_comments=$show_comments&auto_play=$auto_play&color=$color" allowscriptaccess="always">
 </object>
 BASE;
